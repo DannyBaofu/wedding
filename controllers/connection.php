34d841fcfe62b1982734972f5f100f43ff590d7f
Lee  Poh Hock <leepohhock017@gmail.com>
2021-01-30 01:30:10 +0800

    third

--- controllers/connection.php
+++ controllers/connection.php
@@ -1,7 +1,7 @@
 <?php 
 $host = "db4free.net";
 $username = "b2danny";
-$password = "619874388a";
+$password = "619874388";
 $name = "project_wedding";
 
 $cn = new mysqli($host ,$username ,$password,$name);
